0b2ab7876c2d009077bd08a7db807eae921fded1
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2023-11-03 10:13:56 -0500

    testing new branch

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.resolve('./client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "Hello Beautiful World!" });
 });
 
 // All other GET requests not handled before will return our React app
